c5098f42493ac59faf6a31ac39d5eaca06d626cb
Ohran <oki.imamovic@gmail.com>
2022-04-12 02:26:08 +0200

    Update tasks.ts

--- Typescript/ohran/MyPlayground/tasks.ts
+++ Typescript/ohran/MyPlayground/tasks.ts
@@ -286,3 +286,9 @@ export function oneOfKind(arr : number []) : number [] {
 
     return newArr;
 }
+
+export function fibonacci(n : number) : number {
+    if(n <= 2){
+        return 1;
+    }else return fibonacci(n -1) + fibonacci (n - 2);
+}
